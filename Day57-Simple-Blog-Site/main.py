9b8b67f3e8a039659d7a8d73c7ca29e8ece635fb
Aditya Bhere <adityabhere2007@gmail.com>
2026-07-01 16:07:40 +0530

    Complete Day-57 Project

---
+++ Day57-Simple-Blog-Site/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import requests
+from post import Post
+response = requests.get(url="https://api.jsonbin.io/v3/qs/6a44c7fff5f4af5e294bea6a")
+all_posts = response.json().get("record")
+
+all_posts_list = []
+for post in all_posts:
+    post_title = post["title"]
+    post_subtitle = post["subtitle"]
+    post_body = post["body"]
+    post_index = post["id"]
+    all_posts_list.append(Post(post_title, post_subtitle, post_body, post_index))
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html", all_posts=all_posts_list)
+
+@app.route("/post/<int:index>")
+def post(index):
+    return render_template("post.html", post_id=index, all_posts=all_posts_list)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
